b0ac1287ee2cf11068b0c3d1dd9107edbd954949
denzel <seokgyul@lgcns.com>
2023-01-04 19:25:40 +0900

    initial commit

---
+++ src/main/java/springintro/springintrohello/SpringIntroHelloApplication.java
@@ -0,0 +1,13 @@
+package springintro.springintrohello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntroHelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springintro/springintrohello/SpringIntroHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package springintro.springintrohello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
